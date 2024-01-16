0c46bb0a70636009bc53c5452b93dc01e7ee5f28
Jonathan Carlton <carlton.jonathan@icloud.com>
2024-01-16 17:32:29 -0600

    feat - database.
    adding Postgresql DB and connected to backend.
    DB is Docker based.
    Added routes for Basic CRUD functionality

--- server/app.js
+++ server/app.js
@@ -2,25 +2,78 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const app = express();
+const pool = require("./db");
 dotenv.config();
 
+//middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+// app.use(express.urlencoded({extended: false}))
 
-// create
-app.post('/insert', (req, res) => {
+// create a note
+app.post('/notes', async (req, res) => {
+    try {
+        const {description} = req.body;
+        const newNote = await pool.query(
+            "INSERT INTO notes (description) VALUES($1) RETURNING *", 
+            [description]
+        );
 
+        res.json(newNote.rows[0]);
+    } catch (err) {
+        console.error(err.message);    
+    }
 });
 
-// read
-app.get('/getAll', (req, res) => {
-    res.json({
-        success: true
-    });
+// get all notes
+app.get('/notes', async(req, res) => {
+    try {
+        const allNotes = await pool.query("SELECT * FROM notes");
+        res.json(allNotes.rows)
+    } catch (err) {
+        console.error(err.message);
+    }
 })
-// update
 
-// delete
+// get a note
+app.get('/notes/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const note = await pool.query(
+            "SELECT * FROM notes WHERE notes_id = $1", [id]);
+        res.json(note.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
 
-app.listen(process.env.PORT, () => console.log(`App is running on port ${process.env.PORT}.`))
+// update a note
+app.put("/notes/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const updateNote = await pool.query("UPDATE notes SET description = $1 WHERE notes_id = $2", 
+            [description, id] 
+        );
+        res.json("Note updated!")
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+// delete a note
+app.delete("/notes/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteNote = await pool.query("DELETE FROM notes WHERE notes_id = $1", 
+            [id] 
+        );
+        res.json("Note deleted!")
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`App is running on port ${process.env.PORT}.`)
+})
