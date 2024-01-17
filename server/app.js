c77b6f0bc259e93cbfdaa34a3fdd8b3788ea721f
Jonathan Carlton <carlton.jonathan@icloud.com>
2024-01-16 23:33:15 -0600

    refactor - Code cleanup.
    Delete unused functions

--- server/app.js
+++ server/app.js
@@ -8,19 +8,8 @@ dotenv.config();
 //middleware
 app.use(cors());
 app.use(express.json());
-// app.use(express.urlencoded({extended: false}))
 
 // functions 
-function getDate() {
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
-
-    var current_date = mm + '/' + dd + '/' + yyyy;
-    return current_date;
-}
-
 
 // create a note
 app.post('/notes', async (req, res) => {
